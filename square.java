bc0c954118577366bc00d5518ddfc11a7d28c125
SUGANTHISAKTHI <suganthi.scse@ksrce.ac.in>
2017-07-14 17:14:10 +0530

    Add files via upload

---
+++ square.java
@@ -0,0 +1,16 @@
+package Begineer;
+import java.util.*;
+public class square {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int m=1,p;
+		for(int i=0;i<n;i++){
+			m=m*4;
+		}
+		System.out.println(m);
+
+	}
+
+}
