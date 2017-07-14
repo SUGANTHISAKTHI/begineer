a0f61d66dd94e9a471b278fb2747ea593441a2b6
SUGANTHISAKTHI <suganthi.scse@ksrce.ac.in>
2017-07-14 17:23:50 +0530

    Add files via upload

---
+++ ASCII.java
@@ -0,0 +1,16 @@
+package Begineer;
+import java.util.*;
+public class ASCII {
+
+	public static void main(String[] args) {
+		System.out.println("small character");
+	 for(char i='a';i<='z';i++){
+		 System.out.println((int)i);
+	 }
+	 System.out.println("caps character");
+	 for(char i='A';i<='Z';i++){
+		 System.out.println((int)i);
+	 }
+	}
+
+}
